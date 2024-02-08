1c6c932d78ab861d6ed93cc4794639af5500a4fb
lichao <lichao@shopee.com>
2024-02-08 17:07:02 +0800

    feat: get stock data from api

---
+++ src/type.ts
@@ -0,0 +1,17 @@
+export type StockItem = {
+	tradeDate: string;
+	indexCode: string;
+	indexNameCnAll: string;
+	indexNameCn: string;
+	indexNameEnAll: string;
+	indexNameEn: string;
+	open: number;
+	high: number;
+	low: number;
+	close: number;
+	changePct: number;
+	tradingVol: number;
+	tradingValue: number;
+	consNumber: number;
+	peg: number;
+};
